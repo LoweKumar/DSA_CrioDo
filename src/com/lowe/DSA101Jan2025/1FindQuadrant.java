50682c62ffdb1a0997bb21bd17f64739c729f813
Lowe Kumar <lowekr00@gmail.com>
2025-01-20 21:20:09 +0530

    find in which quadrant does this point lie

---
+++ src/com/lowe/DSA101Jan2025/1FindQuadrant.java
@@ -0,0 +1,30 @@
+package com.lowe.DSA101Jan2025;
+import java.util.*;
+
+class FindQuadrant {
+    // NOTE: Please do not modify this function
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        float x = sc.nextFloat();
+        float y = sc.nextFloat();
+
+        int result = findQuadrant(x, y);
+        System.out.println(result);
+
+    }
+
+    // TODO: Implement this method
+    static int findQuadrant(float x, float y) {
+        if(x>0 && y >0){
+            return 1;}
+        else if (x<0 && y>0){
+            return 2;
+        }
+        else if(x<0 && y<0){
+            return 3;
+        }
+        else 
+        return 4;
+    }
+}
