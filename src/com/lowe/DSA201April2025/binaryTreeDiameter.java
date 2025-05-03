c87285d34a80a6162f71a6d6742ccff90f5079df
Lowe Kumar <lowekr00@gmail.com>
2025-05-03 18:15:46 +0530

    binary tree diameter

---
+++ src/com/lowe/DSA201April2025/binaryTreeDiameter.java
@@ -0,0 +1,43 @@
+package com.lowe.DSA201April2025;
+
+import java.util.*;
+import crio.ds.Tree.TreeNode;
+
+/*
+// Definition of TreeNode
+public class TreeNode {
+    public long val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode (long x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
+Use new TreeNode(data) to create new Node
+*/
+public class Solution {
+private int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+}
