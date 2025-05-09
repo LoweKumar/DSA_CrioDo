588567afb74fd9550524f240ceb70808675f0f82
Lowe Kumar <lowekr00@gmail.com>
2025-05-09 21:11:25 +0530

    Kth largest element in an array

---
+++ src/com/lowe/DSA202May2025/KthLargestElementInArray.java
@@ -0,0 +1,86 @@
+package com.lowe.DSA202May2025;
+
+import java.io.*;
+import java.util.*;
+
+class KthLargestElementInAnArray {
+    
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+
+        for (int num : nums) {
+            if (minHeap.size() < k) {
+                minHeap.offer(num);
+            } else if (num > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(num);
+            }
+        }
+
+        return minHeap.peek(); 
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; ++i)
+            nums[i] = scanner.nextInt();
+
+        int k = scanner.nextInt();
+        scanner.close();
+
+        int result = new KthLargestElementInAnArray().findKthLargest(nums, k);
+        System.out.println(result);
+    }
+}
+
+/*
+ * Problem Description
+Given an unsorted array, find the Kth largest element in it.
+
+Note: The need is to find the Kth largest element in the sorted order.
+
+Input format
+First line has the integer N, representing the number of elements in the array
+
+Second line has N integers representing elements of the array
+
+Third line has K, which is the Kth largest element to be found
+
+Output format
+The Kth largest element value needs to be printed
+
+Constraints
+1 <= N <= 1e5
+
+0 <= Array Values <= 1e9
+
+1 ≤ K ≤ N
+
+Sample Input 1
+6
+
+3 2 1 5 6 4
+
+2
+
+Sample Output 1
+5
+
+Explanation 1
+From the given input array, we have to find the second largest value. The first largest is 6 and the second largest is 5, which is the answer.
+
+Sample Input 2
+9
+
+3 2 3 1 2 4 5 5 6
+
+4
+
+Sample Output 2
+4
+
+Explanation 2
+From the given input array, we have to find the fourth largest value. The sorted order with the largest first is 6 5 5 4 3 3 2 2 1. The fourth largest here has the value 4.
+ */
